673ea8faf65f562b021d0127dd41bb6b35b0b4af
ukasz09 <246703@student.pwr.edu.pl>
2021-10-03 19:41:00 +0200

    Reformat css class nesting

--- src/app/shared/components/profile-avatar-logged-user/profile-avatar-logged-user.component.ts
+++ src/app/shared/components/profile-avatar-logged-user/profile-avatar-logged-user.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-profile-avatar-logged-user',
-  templateUrl: './profile-avatar-logged-user.component.html',
-  styleUrls: ['./profile-avatar-logged-user.component.scss'],
+  selector: "app-profile-avatar-logged-user",
+  templateUrl: "./profile-avatar-logged-user.component.html",
+  styleUrls: ["./profile-avatar-logged-user.component.scss"]
 })
 export class ProfileAvatarLoggedUserComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+  constructor() {
+  }
 
   public get loggedUserAvatar(): string {
     // TODO: tmp mocked - use DI injected image from server
-    return '/assets/images/login-page-2.png';
+    return "/assets/images/login-page-2.png";
+  }
+
+  public ngOnInit(): void {
   }
 }
