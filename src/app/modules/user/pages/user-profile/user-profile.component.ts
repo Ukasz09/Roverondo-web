6f766045e94c53a28964be8c594b359e6c47b7a3
unknown <gajerski.lukasz@gmail.com>
2021-11-15 22:33:14 +0100

    Implement user info view

--- src/app/modules/user/pages/user-profile/user-profile.component.ts
+++ src/app/modules/user/pages/user-profile/user-profile.component.ts
@@ -25,7 +25,4 @@ export class UserProfileComponent implements OnInit {
     });
   }
 
-  public userStringify(): string {
-    return JSON.stringify(this.user);
-  }
 }
--- src/app/shared/components/glass-btn/glass-btn.component.ts
+++ src/app/shared/components/glass-btn/glass-btn.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-glass-btn",
@@ -6,6 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./glass-btn.component.scss"]
 })
 export class GlassBtnComponent implements OnInit {
+  @Input() paddingY = "0.5rem";
 
   constructor() {
   }
