14ca5c21ae799ad5b43ca9c4218a41ec6ac59fdc
unknown <gajerski.lukasz@gmail.com>
2021-12-07 19:12:00 +0100

    Fix error snack-bar on user register

--- src/app/core/services/data/current-user.service.ts
+++ src/app/core/services/data/current-user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { BehaviorSubject, throwError, zip } from "rxjs";
+import { BehaviorSubject, Subscription, throwError, zip } from "rxjs";
 import { User } from "@app/core/models";
 import { UsersService } from "./users.service";
 import { AuthService } from "@auth0/auth0-angular";
@@ -11,6 +11,7 @@ import { switchMap } from "rxjs/operators";
 })
 export class CurrentUserService {
   private currentUserSubject$: BehaviorSubject<User | undefined> = new BehaviorSubject<User | undefined>(undefined);
+  private currentUserSubscription$?: Subscription;
 
   constructor(
     private readonly http: HttpClient,
@@ -20,14 +21,10 @@ export class CurrentUserService {
   }
 
   public fetchCurrentUser(): void {
-    zip(
-      this.authService.isAuthenticated$,
-      this.authService.user$
-    ).pipe(
-      switchMap(([authenticated, providerUser]) => {
+    this.currentUserSubscription$ = this.authService.isAuthenticated$.pipe(
+      switchMap((authenticated) => {
         if (authenticated) {
-          const userId = providerUser?.sub as string;
-          return this.usersService.getUserByProvider$(userId);
+          return this.usersService.registerUser$();
         } else return throwError("User not authenticated");
       })
     ).subscribe((user) => this.currentUserSubject$.next(user));
